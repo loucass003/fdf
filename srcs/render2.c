78f6ef754f80cb0d31394d61f44cd705d59b2bd6
Lucas LELIEVRE <llelievr@e1r3p12.42.fr>
2018-12-12 18:17:27 +0100

    je vous emerde et je rentre a ma maison

--- srcs/render2.c
+++ srcs/render2.c
@@ -1,41 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   render.c                                           :+:      :+:    :+:   */
+/*   render2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 21:15:45 by llelievr          #+#    #+#             */
-/*   Updated: 2018/12/12 15:10:58 by llelievr         ###   ########.fr       */
+/*   Updated: 2018/12/12 17:56:40 by llelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "fdf.h"
+#include "fdf.h"
 #include <stdio.h>
 
-static t_bool	project(t_fdf *inst, t_vec3 p, t_zpixel *pi)
+static t_bool	project(t_fdf *inst, t_vec3 p_raster, t_vec3 *raster)
 {
-	int y;
 	t_vec3 vec;
-	p = ft_vec3_sub(p, (t_vec3){inst->map->cols * 0.5, 0, inst->map->rows * 0.5});
-	y = p.y;
-	p = ft_vec3_mul(p, (t_vec3){1, inst->map->a, 1});
-	p = ft_vec3_div(p, (t_vec3){inst->map->cols, 1, inst->map->rows});
-	vec = ft_mat4_mulv(inst->camera->matrix, p);
+	vec = ft_vec3_mul(p_raster, (t_vec3){1, 1, 1});
+	vec = ft_mat4_mulv(inst->camera->matrix, vec);
 	//printf("point -> %f\n", );
 	if (vec.x < -2 || vec.x > 2 || vec.y < -2 || vec.y > 2 || vec.z < -1)
 		return (FALSE);
 
-	*pi = ((t_zpixel){
-		(int)((vec.x + 1) * 0.5 * inst->size.x),
-		(int)((1 - (vec.y + 1) * 0.5) * inst->size.y),
-		-y,
-		0
+	*raster = ((t_vec3){
+		((vec.x + 1) * 0.5 * inst->size.x),
+		((1 - (vec.y + 1) * 0.5) * inst->size.y),
+		-vec.z
 	});
 	return (TRUE);
 }
 
-t_bool			put_pixel(t_fdf *inst, t_zpixel p)
+
+t_bool			put_pixel(t_fdf *inst, t_zpixel p_raster)
 {
 	char 	*img;
 	int		s_pixel;
@@ -44,12 +40,22 @@ t_bool			put_pixel(t_fdf *inst, t_zpixel p)
 
 	if (!(img = mlx_get_data_addr(inst->img, &s_pixel, &s_line, &endian)))
 		return (FALSE);
-	if (p.x < 0 || p.x >= inst->size.x - 1 || p.y < 0 || p.y >= inst->size.y - 1)
+	if (p_raster.x < 0 || p_raster.x >= inst->size.x - 1 || p_raster.y < 0 || p_raster.y >= inst->size.y - 1)
 		return (FALSE);
-	if (p.z_index < inst->p_img[(p.y * inst->size.x) + p.x].z_index)
-		return (FALSE);
-	inst->p_img[(p.y * inst->size.x) + p.x] = p;
+/*	if (p_raster.z_index < inst->p_img[(p_raster.y * inst->size.x) + p_raster.x].z_index)
+		return (FALSE);*/
+	inst->p_img[(p_raster.y * inst->size.x) + p_raster.x] = p_raster;
 	return (TRUE);
+}
+
+float min3(float a, float b, float c) 
+{ 
+	return fmin(a, fmin(b, c));
+}  
+ 
+float max3(float a, float b, float c) 
+{ 
+	return fmax(a, fmax(b, c)); 
 }
 
 void			draw_map(t_fdf *inst)
@@ -58,7 +64,7 @@ void			draw_map(t_fdf *inst)
 	t_line *line;
 	size_t row = 0;
 	size_t col;
-	t_zpixel p;
+	t_vec3 p_raster;
 	t_zpixel p2;
 	t_vec3	a;
 	char 	*img;
@@ -80,41 +86,52 @@ void			draw_map(t_fdf *inst)
 	}
 	if (!!(img = mlx_get_data_addr(inst->img, &s_pixel, &s_line, &endian)))
 		ft_bzero(img, (inst->size.y * s_line));
+	t_line *l_line = NULL;
 	while (lst)
 	{
 		line = lst->content;
 		col = 0;
 		while (col < (inst->map)->cols)
 		{
-			a = (t_vec3){col, -(line->values[col]), row};
-			if (!project(inst, a, &p))
-			{
-				col++;
-				continue ;
-			}
+			t_vec3 a = (t_vec3){col, -(line->values[col]), row};
 			t_line *n_line = lst->next ? (lst->next)->content : NULL;
-			if (col + 1 < (inst->map)->cols)
-			{
-				a = (t_vec3){(col + 1), -(line->values[col + 1]), row};
-				if (!project(inst, a, &p2))
+			if (col + 1 >= (inst->map)->cols)
+				continue ;
+			t_vec3 b = (t_vec3){(col + 1), -(line->values[col + 1]), row};
+			int even = (y % 2 == 0);
+			t_vec3 c = (t_vec3){col + (!even), -(n_line->values[col]), row + 1};
+
+			t_vec3 a_r, b_r, c_r;
+			project(inst, a, &a_r);
+			project(inst, b, &b_r);
+			project(inst, b, &b_r);
+
+			a_r.z = 1 / a_r.z;
+			b_r.z = 1 / b_r.z;
+			c_r.z = 1 / c_r.z;
+
+			t_vec2 born_x = (t_vec2){ min4(a.x, b.x, c.x), max4(a.x, b.x, c.x) };
+			t_vec2 born_y = (t_vec2){ min4(a.y, b.y, c.y), max4(a.y, b.y, c.y) };
+
+			if (born_x.x > inst->size.x - 1 || born_x.y < 0 || born_y.x > inst->size.y - 1 || born_y.y < 0)
+				continue ;
+			
+			int x0 = fmax(0, (int)(floor(born_x.x))); 
+			int x1 = fmin(inst->size.x - 1, (int)(floor(born_x.y))); 
+			int y0 = fmax(0, (int)(floor(born_y.x))); 
+			int y1 = fmin(inst->size.y - 1, (int)(floor(born_y.y)));
+
+			float area = 0;
+			for (int y = y0; y <= y1; ++y) 
+			{ 
+				for(int x = x0; x <= x1; ++x) 
 				{
-					col++;
-					continue ;
+					
 				}
-				draw_line(inst, p, p2);
-			}
-			if (col < (inst->map)->cols && n_line)
-			{
-				a = (t_vec3){col, -(n_line->values[col]), row + 1};
-				if (!project(inst, a, &p2))
-				{
-					col++;
-					continue ;
-				}
-				draw_line(inst, p, p2);
 			}
 			col++;
 		}
+		l_line = line; 
 		lst = lst->next;
 		row++;
 	}
@@ -133,4 +150,3 @@ void			draw_map(t_fdf *inst)
 	}
 	mlx_put_image_to_window(inst->mlx, inst->win, inst->img, 0, 0);
 }
-*/
